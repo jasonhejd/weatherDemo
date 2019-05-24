99a2b434b80ac41b4c0a71bb2ad889e470ca15ee
jasonhejd <he605559281@vip.qq.com>
2019-05-25 00:45:25 +0800

    Forth commit

---
+++ app/src/main/java/com/example/weatherdemo/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.example.weatherdemo.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/com/example/weatherdemo/gson/Suggestion.java
@@ -0,0 +1,32 @@
+package com.example.weatherdemo.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Suggestion {
+
+    @SerializedName("comf")
+    public Comfort comfort;
+
+    @SerializedName("cw")
+    public CarWash carWash;
+
+    public Sport sport;
+
+    private class Comfort {
+
+        @SerializedName("txt")
+        public String info;
+    }
+
+    private class CarWash {
+
+        @SerializedName("txt")
+        public String info;
+    }
+
+    private class Sport {
+
+        @SerializedName("txt")
+        public String info;
+    }
+}
